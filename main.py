187d731f139e9847f4741550a112b8defb1b57f2
Shaharear Rahman <54302299+ShaharearSabbir@users.noreply.github.com>
2021-02-15 04:04:17 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+
+def find_operator(string):
+    for op in string:
+        if op=="+":
+            return op
+        elif op=="-":
+            return op
+        elif op=="*":
+            return op
+        elif op=="/":
+            return op
+
+def calculation(num1,num2,op):
+    if op=="+":
+        return num1+num2;
+    elif op=="-":
+        return num1-num2;
+    elif op=="*":
+        return num1*num2;
+    elif op=="/":
+        return num1/num2;
+
+inp = input("Enter the Celculation: ")
+op=find_operator(inp)
+index=inp.index(op)
+num1=int(inp[0:index])
+num2=int(inp[index:len(inp)])
+print("Result is",calculation(num1,num2,op))
